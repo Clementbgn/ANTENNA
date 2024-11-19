c5c89ea663208c658216e50064d1207c5bdf74f2
Clément Bouguyon <clement.bouguyon@gmail.com>
2024-11-19 01:30:09 +0100

    Modification de Satellite_Position.py changement de passation des arguments (vecteurs) + tentative de conversion au format J2000 mais les conversions sont en angles à modifier ne fonctionne pas pour le moment

--- Code_Python/Satellite_Position.py
+++ Code_Python/Satellite_Position.py
@@ -1,10 +1,13 @@
 #IMPORTS
+import astropy.coordinates
 import pyorbital
-import numpy as np
 from pyorbital.orbital import Orbital
+import numpy as np
 import datetime
 import time
-import json
+import astropy
+import astropy.units as u
+import pandas as pd
 
 # Use current TLEs from the internet: need to edit the file platforms.txt add [Name sat id_norad]
 orb = Orbital("EIRSAT-1")
@@ -22,31 +25,43 @@ table = []
 
 t_prop = 600 #s
 for x in range(0, t_prop):
-    table.append(np.array(orb.get_lonlatalt(now))) #Addition of the x second propagation to the table
+    coord=np.array(orb.get_lonlatalt(now))
+    table.append(coord) #Addition of the x second propagation to the table
     now=now+delta1s #Incrementation of the time
-    print(table[x]) #Display table as the for loop go
+    #print(table[x]) #Display table as the for loop go
+
+table=np.array(table) #List conversion to array table[index of delta T's][0=Lon 1=LAt 2=Alt]
+
+#Convert LongLatAlt to Ra dec
+
+long = np.deg2rad(table[:,0])
+lat = np.deg2rad(table[:,1])
+alt = table[:,2]*1000
+
+Position_geocentric = astropy.coordinates.SkyCoord(long, lat, alt, frame='geocentricmeanecliptic', unit=(u.radian, u.radian, u.meter))
+Position_j2000 = Position_geocentric.transform_to('fk5')
+Table_j2000 = Position_j2000.to_table()
+Table_j2000.remove_column("distance")
+
+print(Table_j2000) #Not working yet
+
+
+
 
 
 
 
 
 '''
-#DEFINITION OF NOW: UTC timezone
-now = datetime.datetime.now(datetime.timezone.utc)
+for x in range (0, int(table.size/3)):
+    y=0
 
-#Obtention of Longitude Latitude and Altitude now
-LongLatAlt = np.array(orb.get_lonlatalt(now))
-print("Position Now: ")
-print(LongLatAlt)
+    long=np.deg2rad(table[x][y])
+    lat=np.deg2rad(table[x][y+1])
+    alt=table[x][y+2]
+    Position_geocentric = astropy.coordinates.SkyCoord(long, lat, alt, frame='geocentricmeanecliptic', unit=(u.radian, u.radian, u.meter))
+    Position_j2000 = Position_geocentric.transform_to('fk5')
+    Table_ra_dec.append(Position_j2000)
 
-#creation of a deltaT
-delta=datetime.timedelta(hours=1)
-
-#Addition of the deltaT to t0(old now)
-dans1h=now+delta
-
-#Print Longitude Latitude and Altitude at now+deltaT
-LongLatAlt1h = np.array(orb.get_lonlatalt(dans1h))
-print("Position in 1 hour: ")
-print(LongLatAlt1h)
+Table_ra_dec = np.array(Table_ra_dec)
 '''
