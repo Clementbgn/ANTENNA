20550af529937af6c2b3526195f13bb3be8c056d
Clément Bouguyon <clement.bouguyon@gmail.com>
2025-01-12 18:06:37 +0100

    Renaming of the function in realtime_position for easier understanding

--- Code_Python/realtime_position.py
+++ Code_Python/realtime_position.py
@@ -2,40 +2,41 @@ from skyfield.api import EarthSatellite, Topos, load
 from skyfield.api import wgs84
 import datetime
 import time
-import check_visibility
+import Satellite_Loader
+import Antenna_Site
 
 
-satellite = check_visibility.satellite
-antenna_site = check_visibility.antenna_site
-Satellite_name = check_visibility.Satellite_name
+satellite = Satellite_Loader.satellite
+antenna_site = Antenna_Site.antenna_site
+Satellite_name = Satellite_Loader.Satellite_name
 ts = load.timescale()
 
-def get_geocentric_position_once():
+def get_geocentric_position():
     t = ts.now()
     geocentric = satellite.at(t)
 
     return geocentric
 
-def get_geocentric_postition_rt():
+def print_geocentric_postition():
     while True:
-        geocentric = get_geocentric_position_once()
+        geocentric = get_geocentric_position()
         print(geocentric)
         time.sleep(1)
 
-def get_latitude_longitude_once():
-    geocentric = get_geocentric_position_once()
+def get_latitude_longitude():
+    geocentric = get_geocentric_position()
     lat_sat, lon_sat = wgs84.latlon_of(geocentric)
 
     return lat_sat, lon_sat
 
-def get_latitude_longitude_rt():
+def print_latitude_longitude_rt():
     while True:
-        lat_sat, lon_sat = get_latitude_longitude_once()
+        lat_sat, lon_sat = get_latitude_longitude()
         print('Latitude:', lat_sat)
         print('Longitude:', lon_sat,"\n")
         time.sleep(1)
 
-def get_relative_position_antenna_satellite_AltAz_once():
+def get_relative_position_antenna_satellite_AltAz():
     t=ts.now()
     relative_position_satellite_antenna = satellite - antenna_site
     topocentric = relative_position_satellite_antenna.at(t)
@@ -43,16 +44,16 @@ def get_relative_position_antenna_satellite_AltAz_once():
 
     return alt_sat, az_sat, distance_sat
 
-def get_relative_position_antenna_satellite_AltAz_rt():
+def print_relative_position_antenna_satellite_AltAz():
     while True:
-        alt_sat, az_sat, distance_sat = get_relative_position_antenna_satellite_AltAz_once()
+        alt_sat, az_sat, distance_sat = get_relative_position_antenna_satellite_AltAz()
         print("Elevation of ",Satellite_name," in the sky is: ",alt_sat.degrees)
         print("Azimuth of ",Satellite_name," in the sky is: ", az_sat.degrees)
         print("Distance of ",Satellite_name," in the sky is: ", distance_sat.km, "\n")
 
         time.sleep(1)
 
-def get_relative_position_antenna_satellite_RaDec_once():
+def get_relative_position_antenna_satellite_RaDec():
     t=ts.now()
     relative_position_satellite_antenna = satellite - antenna_site
     topocentric = relative_position_satellite_antenna.at(t)
@@ -60,9 +61,9 @@ def get_relative_position_antenna_satellite_RaDec_once():
 
     return ra_sat, dec_sat, distance_sat_radec
 
-def get_relative_position_antenna_satellite_RaDec_rt():
+def print_relative_position_antenna_satellite_RaDec():
     while True:
-        ra_sat, dec_sat, distance_sat_radec = get_relative_position_antenna_satellite_RaDec_once()
+        ra_sat, dec_sat, distance_sat_radec = get_relative_position_antenna_satellite_RaDec()
         print('RA of ' + Satellite_name + " in the sky:", ra_sat)
         print('DEC of ' + Satellite_name + " in the sky:", dec_sat, "\n")
         time.sleep(1)
@@ -76,6 +77,9 @@ def where_relative_horizon():
             print( Satellite_name + ' is below the horizon',"\n")
         time.sleep(1)
 
+#MAIN PART OF THE CODE
+
+print_relative_position_antenna_satellite_AltAz()
 
 
 
